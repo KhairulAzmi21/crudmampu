56b540479b895ba7f0b5185b72f7847e1ddd8a12
Khairul Azmi <KA@KA-Macbook.local>
2018-03-08 15:22:30 +0800

    latest code

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    public function post_information()
+    {
+        return $this->hasOne(PostInformation::class)
+                    ->withDefault([
+                        'hashtag' => 'tiada value',
+                        'color'   => '#000000',
+                    ]);
+    }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -8,6 +8,16 @@
                     <span class="float-right">
                         {{ $post->created_at->diffForHumans() }}
                     </span>
+                    <h6>
+                        <!--
+                        cara panggil one-to-one
+                        $post point kepada nama_method tanpa () kemudian nama column dalam DB
+                        contohnya $post->post_information->color
+                         -->
+                        #<span style="color:{{ $post->post_information->color}};font-weight:bold">
+                                {{ $post->post_information->hashtag}}
+                         </span>
+                    </h6>
             </div>
             <div class="card-body">
                 {{ $post->body }}
